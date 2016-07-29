9ad95376950446455f514f44e1c36d9f88f02906
c9s <yoanlin93@gmail.com>
2016-07-30 07:29:22 +0800

    Extract DEPLOY_PREFIX and support --no-clean option

--- src/bin/typeloy.ts
+++ src/bin/typeloy.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import path = require('path');
 import {readConfig} from '../config';
-import ActionsRegistry from '../actions';
+import Actions from '../actions';
+import {CmdDeployOptions} from '../options';
 require('colors');
 
 var prog = require('commander');
@@ -10,25 +11,34 @@ var version = '1.0.0';
 var cwd = path.resolve('.');
 
 // read config and validate it
-var config = readConfig();
-var actionsRegistry = new ActionsRegistry(config, cwd);
 
 prog.version(version);
 prog.usage('[options] <subcommand> ...');
 prog.option('-v, --verbose', 'verbose mode');
+prog.option('-c, --config <file>', 'config file');
+
 
 prog.command('deploy [version] [sites...]')
   .description('set the deployment version and start deploying.')
   .option("-d, --dryrun", 'do not really deploy it.')
-  .action( (version, sites, options) => {
-    actionsRegistry.deploy(options);
+  .option("--bundle-file <file>", 'the bundle file you have already built with meteor build.')
+  .option("-C, --no-clean", 'whether to clean up the bundle files.')
+  .action((version:string, sites:Array<string>, options:CmdDeployOptions) => {
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    if (!version) {
+      version = "v" + (new Date).getTime();
+    }
+    actions.deploy(version, sites, options);
   })
   ;
 
 prog.command('setup')
   .description('setup the requirements on the target server.')
   .action( (env, options) => {
-    actionsRegistry.setup();
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.setup();
   })
   ;
 
@@ -36,33 +46,43 @@ prog.command('logs')
   .description('init the mup.json config.')
   .option("-f, --tail", 'tail')
   .action((options) => {
-    actionsRegistry.logs(options);
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.logs(options);
   });
 
 prog.command('init')
   .description('init the mup.json config.')
   .action( (env, options) => {
-    actionsRegistry.init();
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.init();
   });
 
 prog.command('start')
   .description('start the app.')
   .action( (env, options) => {
-    actionsRegistry.start();
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.start();
   });
   ;
 
 prog.command('stop')
   .description('stop the app.')
   .action( (env, options) => {
-    actionsRegistry.stop();
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.stop();
   });
   ;
 
 prog.command('restart')
   .description('restart the app.')
   .action( (env, options) => {
-    actionsRegistry.restart();
+    let config = readConfig(prog.config);
+    let actions = new Actions(config, cwd);
+    actions.restart();
   });
   ;
 
