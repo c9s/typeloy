a74d813e9a12b7f382d354649d27a40d7e7a6e48
c9s <yoanlin93@gmail.com>
2016-07-29 21:12:16 +0800

    Add typings

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/colors/index.d.ts" />
+/// <reference path="globals/commander/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
+/// <reference path="modules/object-assign/index.d.ts" />
