f11bb3deac66a3dfad9f01c186b83e2a82406524
c9s <yoanlin93@gmail.com>
2016-08-04 18:17:26 +0800

    Add config tests

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/archiver/index.d.ts" />
 /// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/commander/index.d.ts" />
 /// <reference path="globals/debug/index.d.ts" />
 /// <reference path="globals/express-serve-static-core/index.d.ts" />
