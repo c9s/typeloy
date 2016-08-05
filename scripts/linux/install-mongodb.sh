9309e80a6f993688286cde590bdf08105a4a2e8b
c9s <yoanlin93@gmail.com>
2016-08-05 18:07:18 +0800

    Extract apt-get update task out

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -9,7 +9,6 @@ set -e
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update -y
 sudo apt-get install mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-tools -y
 
 # Restart mongodb
