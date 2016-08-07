1f318a8ae0f461c152ca47e4458b20413fd9f301
c9s <yoanlin93@gmail.com>
2016-08-07 10:22:01 +0800

    Improve git message parser

--- src/GitSync.ts
+++ src/GitSync.ts
@@ -12,8 +12,59 @@ export class GitCommit {
   date: Date;
 }
 
+
+function qq(val : string) : string {
+  return "\"" + val.replace(/["]/, "\\\"") + "\"";
+}
+
+function q(val : string) : string {
+  return "'" + val.replace(/[']/, "\\'") + "'";
+}
+
+const GitCommitSpliter = new RegExp('^commit\\s(?=\\w{40})', 'gm');
+
+const GitCommitHashRegExp = new RegExp("(\\w{40})");
+const GitCommitAuthorRegExp = new RegExp("^Author: (.*)\\s+<(.*?)>");
+const GitCommitDateRegExp = new RegExp("^Date: (.*)$");
+
 export class GitSync {
   constructor() {
+
+  }
+
+  /**
+   * parse commit logs from text
+   */
+  protected parseCommitLogs(output:string) : Array<GitCommit> {
+    let commits = output.trim().split(GitCommitSpliter);
+    commits.shift(); // skip empty chunk
+
+    return commits.map((text) : GitCommit => {
+      let lines = text.split(/\r?\n/);
+      let commit:GitCommit = {} as GitCommit;
+      let line, matches;
+      
+      line = lines.shift();
+      if (matches = line.match(GitCommitHashRegExp)) {
+        commit.hash = matches[1];
+      }
+
+      line = lines.shift();
+      if (matches = line.match(GitCommitAuthorRegExp)) {
+        commit.author = { name: matches[1], email: matches[2] } as GitAuthor;
+      }
+
+      line = lines.shift();
+      if (matches = line.match(GitCommitDateRegExp)) {
+        commit.date = new Date(matches[1]);
+      }
+      lines.shift(); // ignore first empty line
+      while (lines[lines.length - 1].match(/^\s*$/)) {
+        lines.pop();
+      }
+      commit.message = lines.map((line:string) => line.replace(/^\s{4}/,'') ).join("\n");
+      return commit;
+    });
 
   }
 
@@ -22,58 +73,35 @@ export class GitSync {
     let cmdopts = [];
     let val;
     if (val = options.maxCount) {
-      cmdopts.push(`--max-count=${val}`);
+      cmdopts.push('--max-count', val);
     }
     if (val = options.skip) {
-      cmdopts.push(`--skip=${val}`);
+      cmdopts.push('--skip', val);
     }
     if (val = options.until) {
-      cmdopts.push(`--until=${val}`);
+      cmdopts.push('--until', val);
     }
     if (val = options.author) {
-      cmdopts.push(`--author=${val}`);
+      cmdopts.push('--author', val);
+    }
+    if (val = options.committer) {
+      cmdopts.push('--committer', val);
     }
     if (val = options.grep) {
-      cmdopts.push(`--grep=${val}`);
+      cmdopts.push('--grep', val);
     }
 
     let output = child_process.execSync(`git log ${ref} ${cmdopts.join(' ')}`, {
       "encoding": "utf8"
     });
-    let commits = output.trim().split(/commit\s/);
-    commits.shift();
-    return commits.map((text) : GitCommit => {
-      let lines = text.split(/\r?\n/);
-      let commit:GitCommit = {} as GitCommit;
-      let line, matches;
-      
-      line = lines.shift();
-      if (matches = line.match(/^(\w{40})/)) {
-        commit.hash = matches[1];
-      }
-
-      line = lines.shift();
-      if (matches = line.match(/^Author: (.*)\s+<(.*?)>/)) {
-        commit.author = { name: matches[1], email: matches[2] } as GitAuthor;
-      }
-
-      line = lines.shift();
-      if (matches = line.match(/^Date: (.*)$/)) {
-        commit.date = new Date(matches[1]);
-      }
-      commit.message = lines.map((line:string) => line.replace(/^\s{4}/,'') ).join("\n");
-      return commit;
-    });
+    return this.parseCommitLogs(output);
   }
 
   public logSince(since, til) {
     let output = child_process.execSync(`git log ${since}..${til}`, {
       "encoding": "utf8"
     }).trim();
-
-
-
-
+    return this.parseCommitLogs(output);
   }
 
 
