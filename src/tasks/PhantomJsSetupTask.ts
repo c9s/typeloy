1fceae28d768bb695b86cc8a8393310afd9f7697
c9s <yoanlin93@gmail.com>
2016-09-12 17:41:41 +0800

    More task classes

---
+++ src/tasks/PhantomJsSetupTask.ts
@@ -0,0 +1,20 @@
+import {SCRIPT_DIR, TEMPLATES_DIR} from "./Task";
+import {SetupTask} from "./SetupTask";
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+export class PhantomJsSetupTask extends SetupTask {
+
+  public describe() : string {
+    return 'Installing PhantomJS';
+  }
+  
+  public build(taskList) {
+    taskList.executeScript(this.describe(), {
+      script: path.resolve(SCRIPT_DIR, 'install-phantomjs.sh')
+    });
+  }
+
+}
