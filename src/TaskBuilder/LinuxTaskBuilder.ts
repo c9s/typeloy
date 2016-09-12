745e47a320766c7b7205080c4378b16d2697026c
c9s <yoanlin93@gmail.com>
2016-09-12 18:06:55 +0800

    Refactor task classes

--- src/TaskBuilder/LinuxTaskBuilder.ts
+++ src/TaskBuilder/LinuxTaskBuilder.ts
@@ -24,7 +24,8 @@ import {
   EnvVarsTask,
   BashEnvVarsTask,
   DeployTask,
-  StartProcessTask
+  StartProcessTask,
+  CopyBundleDeployTask
 } from "../tasks";
 
 function translateBackupMongoConfigVars(config : Config) : any {
@@ -41,32 +42,6 @@ function translateBackupMongoConfigVars(config : Config) : any {
 }
 // 'backupMongo': translateBackupMongoConfigVars(this.config),
 
-
-
-class CopyBundleDeployTask extends DeployTask {
-
-  protected bundlePath : string;
-
-  constructor(config : Config, bundlePath:string) {
-    super(config);
-    this.bundlePath = bundlePath;
-  }
-
-  public describe() : string {
-    return 'Uploading bundle: ' + this.bundlePath;
-  }
-
-  public build(taskList) {
-    const appName = this.config.app.name;
-    const remoteBundlePath = this.deployPrefix + '/' + appName + '/tmp/bundle.tar.gz'
-    console.log("Transfering " + this.bundlePath + ' => ' + remoteBundlePath);
-    taskList.copy(this.describe(), {
-      src: this.bundlePath,
-      dest: this.deployPrefix + '/' + appName + '/tmp/bundle.tar.gz',
-      progressBar: this.config.enableUploadProgressBar
-    });
-  }
-}
 
 export default class LinuxTaskBuilder extends BaseTaskBuilder {
 
