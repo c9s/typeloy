c3f9a2e3bb6979d5e43bd8f81f93cac41be402d2
c9s <yoanlin93@gmail.com>
2016-11-21 15:13:50 +0800

    Add aws script

---
+++ scripts/aws/install-mongo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat <<END > /etc/yum.repos.d/mongodb-org-3.0.repo
+[mongodb-org-3.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.0/x86_64/
+gpgcheck=0
+enabled=1
+END
+sudo yum update -y
+sudo yum install -y mongodb-org
+sudo service mongod start
+sudo chkconfig mongod on
