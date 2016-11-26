a7ade40f3eeca497e4a8881568c336e83b117386
c9s <yoanlin93@gmail.com>
2016-11-26 14:58:29 +0800

    Rewrite StudSetupTask

--- src/tasks/StudSetupTask.ts
+++ src/tasks/StudSetupTask.ts
@@ -22,28 +22,43 @@ export class StudSetupTask extends SetupTask {
   }
 
   public build(taskList) {
-    this.installStud(taskList);
+    taskList.executeScript('Installing Stud', {
+      script: path.resolve(SCRIPT_DIR, 'stud-install.sh')
+    });
     this.configureStud(taskList, this.sslConfig.pem, this.sslConfig.backendPort);
   }
 
   public run(session : Session) : Promise<SessionResult> {
     const vars = this.extendArgs({});
-    // TODO
-    return sync(
-      executeScript(session, path.resolve(SCRIPT_DIR, 'stud-install.sh'), { vars }),
-      copy(session, path.resolve(TEMPLATES_DIR, 'stud.init.conf'), '/etc/init/stud.conf', { vars }),
-      copy(session, path.resolve(TEMPLATES_DIR, 'stud.service'), '/etc/systemd/system/stud.service', { vars }),
-    );
+    const pemFilePath = this.sslConfig.pem;
+    const port = this.sslConfig.backendPort || 80;
+    const backend = { host: '127.0.0.1', port: port };
+
+    const tasks = [];
+    tasks.push(executeScript(session, path.resolve(SCRIPT_DIR, 'stud-install.sh'), { vars }));
+
+    // Creating upstart service entry
+    tasks.push(copy(session, path.resolve(TEMPLATES_DIR, 'stud.init.conf'), '/etc/init/stud.conf', { vars }));
+
+    tasks.push(copy(session, path.resolve(TEMPLATES_DIR, 'stud.service'), '/etc/systemd/system/stud.service', { vars }));
+
+    if (pemFilePath) {
+      tasks.push(copy(session, path.resolve(pemFilePath), this.deployPrefix + '/stud/ssl.pem', { vars }));
+    }
+
+    // Configuring stud
+    tasks.push(copy(session, path.resolve(TEMPLATES_DIR, 'stud.conf'), this.deployPrefix + '/stud/stud.conf', {
+      vars: { backend: util.format('[%s]:%d', backend.host, backend.port) }
+    }));
+
+    // Verifying SSL Configurations
+    tasks.push(executeScript(session, path.resolve(SCRIPT_DIR, 'stud-restart.sh'), { vars }));
+    return sync(tasks);
   }
 
-  public installStud(taskList) {
-    taskList.executeScript('Installing Stud', {
-      script: path.resolve(SCRIPT_DIR, 'stud-install.sh')
-    });
-  }
 
   public configureStud(taskList, pemFilePath, port = 80) {
-    const backend = {host: '127.0.0.1', port: port};
+    const backend = { host: '127.0.0.1', port: port };
 
     taskList.copy('Creating upstart service entry', {
       src: path.resolve(TEMPLATES_DIR, 'stud.init.conf'),
