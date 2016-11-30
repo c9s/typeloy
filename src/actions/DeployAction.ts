d0a59b484c37194adaed165010bcda9baa24680f
c9s <yoanlin93@gmail.com>
2016-11-30 15:25:16 +0800

    refactor site deploy

--- src/actions/DeployAction.ts
+++ src/actions/DeployAction.ts
@@ -24,6 +24,65 @@ const getDefaultBuildDirName = function(appName : string, tag : string) : string
 
 export class DeployAction extends BaseAction {
 
+  protected deploySite(deployment : Deployment, site : string, bundlePath : string) : Promise<SummaryMap> {
+      const siteConfig = this.getSiteConfig(site);
+      const sessionsMap = this.createSiteSessionsMap(siteConfig);
+      this.log(`Connecting to the ${site} servers: [${ _.map(siteConfig.servers, (server) => server.host).join(', ')}]`);
+
+      // Get settings.json into env,
+      // The METEOR_SETTINGS can be used for setting up meteor application without passing "--settings=...."
+      //
+      // Here is the guide of using METEOR_SETTINGS
+      // https://themeteorchef.com/snippets/making-use-of-settings-json/#tmc-using-settingsjson
+      //
+      // @see http://joshowens.me/environment-settings-and-security-with-meteor-js/
+      //
+      // TODO: support reading settings from command line
+      const meteorSettings = _.extend({
+        "public": {},
+        "private": {},
+        "log": { "level": "warn" }
+      }, siteConfig.settings || this.config.app.settings || {}); // prefer site config over the app settings
+
+      // always update
+      if (this.config.deploy.exposeSiteName && meteorSettings['public'] && typeof meteorSettings['public']['site'] === "undefined") {
+        // XXX: apply siteName from siteConfig
+        meteorSettings['public']['site'] = siteConfig.siteName || site;
+      }
+      if (this.config.deploy.exposeVersionInfo) {
+        meteorSettings['public']['version'] = deployment.brief();
+      }
+
+      console.log("Updated Meteor Settings:");
+      console.log(JSON.stringify(meteorSettings, null, "  "));
+
+      siteConfig.env['METEOR_SETTINGS'] = JSON.stringify(meteorSettings);
+
+      // An array of Promise<SummaryMap> for sites server
+      const groupPromises : Array<Promise<SummaryMap>> 
+        = _.map(sessionsMap, (sessionGroup : SessionGroup) => {
+          const taskBuilder = this.createTaskBuilderByOs(sessionGroup);
+          const sessionPromises : Array<Promise<SummaryMap>> = _.map(sessionGroup.sessions, (session : Session) : Promise<SummaryMap> => {
+              const env = _.extend({},
+                  this.config.env || {},
+                  siteConfig.env || {},
+                  session._serverConfig.env || {});
+              if (typeof env['ROOT_URL'] === "undefined") {
+                console.warn("**WARNING** ROOT_URL is undefined.");
+              }
+              const tasks = taskBuilder.deploy(this.config, bundlePath, env);
+              const runner = new SessionRunner;
+              return runner.execute(session, tasks, {});
+          });
+          return Promise.all(sessionPromises).then((summaryMaps : Array<SummaryMap>) => {
+              return Promise.resolve(mergeSummaryMap(summaryMaps));
+          });
+      });
+      return Promise.all(groupPromises).then((summaryMaps : Array<SummaryMap>) => {
+          return Promise.resolve(mergeSummaryMap(summaryMaps));
+      });
+  }
+
   public run(deployment : Deployment, sites : Array<string>, options : CmdDeployOptions = {} as CmdDeployOptions) {
     console.log(this.config.app);
     const appName = this.config.app.name || "app";
@@ -49,72 +108,10 @@ export class DeployAction extends BaseAction {
       // We only want to fire once for now.
       this.whenBeforeDeploying(deployment);
 
-      let sitesPromise = Promise.resolve({});
-      for (let i = 0; i < sites.length ; i++) {
-        const site = sites[i];
-        sitesPromise = sitesPromise.then((previousSummaryMap : SummaryMap) => {
-          const siteConfig = this.getSiteConfig(site);
-          const sessionsMap = this.createSiteSessionsMap(siteConfig);
-          this.log(`Connecting to the ${site} servers: [${ _.map(siteConfig.servers, (server) => server.host).join(', ')}]`);
-
-          // Get settings.json into env,
-          // The METEOR_SETTINGS can be used for setting up meteor application without passing "--settings=...."
-          //
-          // Here is the guide of using METEOR_SETTINGS
-          // https://themeteorchef.com/snippets/making-use-of-settings-json/#tmc-using-settingsjson
-          //
-          // @see http://joshowens.me/environment-settings-and-security-with-meteor-js/
-          //
-          // TODO: support reading settings from command line
-          const meteorSettings = _.extend({
-            "public": {},
-            "private": {},
-            "log": { "level": "warn" }
-          }, siteConfig.settings || this.config.app.settings || {}); // prefer site config over the app settings
-
-          // always update
-          if (this.config.deploy.exposeSiteName && meteorSettings['public'] && typeof meteorSettings['public']['site'] === "undefined") {
-            // XXX: apply siteName from siteConfig
-            meteorSettings['public']['site'] = siteConfig.siteName || site;
-          }
-          if (this.config.deploy.exposeVersionInfo) {
-            meteorSettings['public']['version'] = deployment.brief();
-          }
-
-          console.log("Updated Meteor Settings:");
-          console.log(JSON.stringify(meteorSettings, null, "  "));
-
-          siteConfig.env['METEOR_SETTINGS'] = JSON.stringify(meteorSettings);
-
-          // An array of Promise<SummaryMap> for sites server
-          const groupPromises : Array<Promise<SummaryMap>>
-            = _.map(sessionsMap, (sessionGroup : SessionGroup) => {
-                const taskBuilder = this.createTaskBuilderByOs(sessionGroup);
-                const sessionPromises = _.map(sessionGroup.sessions,
-                  (session : Session) => {
-                    const env = _.extend({},
-                        this.config.env || {},
-                        siteConfig.env || {},
-                        session._serverConfig.env || {});
-                    if (typeof env['ROOT_URL'] === "undefined") {
-                      console.warn("**WARNING** ROOT_URL is undefined.");
-                    }
-                    const tasks = taskBuilder.deploy(this.config, bundlePath, env);
-                    const runner = new SessionRunner;
-                    return runner.execute(session, tasks, {});
-                  });
-                return Promise.all(sessionPromises).then((summaryMaps) => {
-                  return Promise.resolve(mergeSummaryMap(summaryMaps));
-                });
-              });
-          return Promise.all(groupPromises).then((summaryMaps) => {
-            return Promise.resolve(
-              _.extend(previousSummaryMap, mergeSummaryMap(summaryMaps))
-            );
-          });
-        });
-      }
-      return sitesPromise.then((summaryMap : SummaryMap) => {
+      const sitesDeploy = _.map(sites, (site : string) => this.deploySite(deployment, site, bundlePath));
+      return Promise.all(sitesDeploy).then((summaryMaps : Array<any>) => {
+          return Promise.resolve(mergeSummaryMap(summaryMaps));
+      }).then((summaryMap : SummaryMap) => {
         this.pluginRunner.whenAfterDeployed(deployment);
         if (options.clean) {
           this.log(`Cleaning up ${buildLocation}`);
