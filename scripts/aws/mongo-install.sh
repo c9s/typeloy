dd8f47195cda80a3f424e516511646e9e712a895
c9s <yoanlin93@gmail.com>
2016-12-02 23:54:44 +0800

    Fix mongo install script

--- scripts/aws/mongo-install.sh
+++ scripts/aws/mongo-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
-cat <<END > /etc/yum.repos.d/mongodb-org-3.0.repo
+cat <<END > mongodb-org-3.0.repo
 [mongodb-org-3.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.0/x86_64/
 gpgcheck=0
 enabled=1
 END
+sudo mv -v mongodb-org-3.0.repo /etc/yum.repos.d/mongodb-org-3.0.repo
 sudo yum install -y mongodb-org
 sudo service mongod start
 sudo chkconfig mongod on
