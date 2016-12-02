b0785e8fde7a8d4e191fa4dd709007261fb061d3
c9s <yoanlin93@gmail.com>
2016-12-02 16:34:12 +0800

    refactor

--- src/actions/LogsAction.ts
+++ src/actions/LogsAction.ts
@@ -27,12 +27,13 @@ export class LogsAction extends BaseAction {
   protected runSite(deployment : Deployment, site : string, options : LogsOptions) : Promise<SummaryMap> {
       const siteConfig = this.getSiteConfig(site);
       const sessionsMap = this.createSiteSessionsMap(siteConfig);
+      const runner = new SessionRunner;
+
       const groupPromises : Array<Promise<SummaryMap>> = _.map(sessionsMap, (sessionGroup : SessionGroup, os : string) => {
           const taskListsBuilder = this.createTaskBuilderByOs(sessionGroup);
-          const sessionPromises : Array<Promise<SummaryMap>> = sessionGroup.sessions.map((session : Session) => {
+          const sessionPromises : Array<Promise<SummaryMap>> = _.map(sessionGroup.sessions, (session : Session) => {
               const hostPrefix = `(${site}) [${session._host}] `;
               const tasks = taskListsBuilder.logs(this.config, hostPrefix, options);
-              const runner = new SessionRunner;
               return runner.execute(session, tasks, {});
           });
           return reduceSummaryMaps(sessionPromises);
