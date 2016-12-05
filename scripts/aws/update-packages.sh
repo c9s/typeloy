974ddc4e6c93157a4a06783324cc3060f43ab8e5
c9s <yoanlin93@gmail.com>
2016-12-05 12:01:52 +0800

    enable yum epel repo

--- scripts/aws/update-packages.sh
+++ scripts/aws/update-packages.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+if [[ -e /etc/yum.repos.d/epel.repo ]] ; then
+    sudo sed -i -e 's/enabled=0/enabled=1/' /etc/yum.repos.d/epel.repo
+fi
 sudo yum update -y
 sudo yum upgrade -y
